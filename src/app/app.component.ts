fd1b99f706cf144a8b10d57f30f1e6505253384e
Administrator <caiguo0421@163.com>
2018-04-13 17:15:05 +0800

    angular 学习第二章 数据绑定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: '<h1>{{title}}</h1>' +
+  '<h2>My favorite hero is {{myHero.name}}</h2><p>Heroes:</p>' +
+  '<ul><li *ngFor="let hero of heroes">{{hero.name}}</li>' +
+  '<p *ngIf="heroes.length > 3">There are many heroes!</p>' +
+  '</ul>'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heros';
+  // myHero = 'Windstorm';
+  // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+  heroes = [new Hero(1, 'Windstorm'),
+    new Hero(2, 'Bombasto'),
+    new Hero(3, 'Magneta'),
+    new Hero(4, 'Tornado')
+  ];
+
+  myHero = this.heroes[0];
 }
